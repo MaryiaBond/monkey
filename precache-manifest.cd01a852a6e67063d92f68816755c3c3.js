c2a6f7acc5eed5eeecee57026e8d4adbe4edb9b1
Maria <dairymolko@gamil.com>
2019-12-06 15:32:06 +0300

    Updates

---
+++ precache-manifest.cd01a852a6e67063d92f68816755c3c3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d977ac8dd85574948d6300ce68df5a",
+    "url": "/monkey/index.html"
+  },
+  {
+    "revision": "f719940d64604711c93f",
+    "url": "/monkey/static/css/4.8b928438.chunk.css"
+  },
+  {
+    "revision": "5744d02c1e73b4e8c038",
+    "url": "/monkey/static/css/5.1bbd4572.chunk.css"
+  },
+  {
+    "revision": "ed26b96060c1e275d905",
+    "url": "/monkey/static/css/main.04cabfb3.chunk.css"
+  },
+  {
+    "revision": "150ea092c0932a52e14c",
+    "url": "/monkey/static/js/2.3f3c3e14.chunk.js"
+  },
+  {
+    "revision": "d83e56fc59a818dd509f",
+    "url": "/monkey/static/js/3.4b09bc55.chunk.js"
+  },
+  {
+    "revision": "f719940d64604711c93f",
+    "url": "/monkey/static/js/4.c4f9a677.chunk.js"
+  },
+  {
+    "revision": "5744d02c1e73b4e8c038",
+    "url": "/monkey/static/js/5.97523f90.chunk.js"
+  },
+  {
+    "revision": "ed26b96060c1e275d905",
+    "url": "/monkey/static/js/main.5c3bc9e8.chunk.js"
+  },
+  {
+    "revision": "3e6a170ee71c4b7db6b1",
+    "url": "/monkey/static/js/runtime~main.45fc9e2c.js"
+  },
+  {
+    "revision": "edb8bdd3851cede37b4b28038b2a1973",
+    "url": "/monkey/static/media/als_hauss-black-webfont.edb8bdd3.ttf"
+  },
+  {
+    "revision": "98ee53cf695d7a85d9201fc43d1b33d1",
+    "url": "/monkey/static/media/als_hauss-medium-webfont.98ee53cf.ttf"
+  },
+  {
+    "revision": "d8336f1f72f548df918c488497e86023",
+    "url": "/monkey/static/media/als_hauss-thin-webfont.d8336f1f.ttf"
+  },
+  {
+    "revision": "95546ded1ca6a26ebb14a007cdbd7664",
+    "url": "/monkey/static/media/arrowDown.95546ded.png"
+  },
+  {
+    "revision": "d4fb7c996b2a9e8b9827b2b3083aa332",
+    "url": "/monkey/static/media/arrowLeft.d4fb7c99.png"
+  },
+  {
+    "revision": "50b7e3139313d1262348c52f022478c4",
+    "url": "/monkey/static/media/arrowRight.50b7e313.png"
+  },
+  {
+    "revision": "c8edc239dbc927b6aa0a7aa562b0f69b",
+    "url": "/monkey/static/media/arrowTop.c8edc239.png"
+  },
+  {
+    "revision": "9026aa4eaf87bbc31e43a1edea7c6c80",
+    "url": "/monkey/static/media/avatar1.9026aa4e.jpg"
+  },
+  {
+    "revision": "ccd84487ef55ab163ba06169b5093053",
+    "url": "/monkey/static/media/avatar2.ccd84487.jpg"
+  },
+  {
+    "revision": "2f1d5459fbe98cfc2524398dccf62ff5",
+    "url": "/monkey/static/media/backLevel1.2f1d5459.png"
+  },
+  {
+    "revision": "769eb3212683eb4c8137266047113790",
+    "url": "/monkey/static/media/catEat1.769eb321.gif"
+  },
+  {
+    "revision": "b27f28f6b7db5f78c4d905ee6ebfad70",
+    "url": "/monkey/static/media/food.b27f28f6.png"
+  },
+  {
+    "revision": "bf95e5d81d5b01159094c7748d19a5e3",
+    "url": "/monkey/static/media/gingerSpaceCat.bf95e5d8.jpg"
+  },
+  {
+    "revision": "e8e94dc9bc0b96dd7e41c0c450014f0f",
+    "url": "/monkey/static/media/hungryCat.e8e94dc9.jpg"
+  },
+  {
+    "revision": "d79a46f3be537129b4b7db2c47c37e61",
+    "url": "/monkey/static/media/level2.d79a46f3.png"
+  },
+  {
+    "revision": "66f9997c241a9272d21df4b039ee7aeb",
+    "url": "/monkey/static/media/like.66f9997c.svg"
+  },
+  {
+    "revision": "2eb49705bd714ea63af7286662bbbd07",
+    "url": "/monkey/static/media/loader.2eb49705.svg"
+  },
+  {
+    "revision": "ad893845f0d5d335e0691156409b068b",
+    "url": "/monkey/static/media/space_monkey_logo.ad893845.jpg"
+  },
+  {
+    "revision": "5c1af95e7d29b17c0be558b78add1a32",
+    "url": "/monkey/static/media/user.5c1af95e.jpg"
+  },
+  {
+    "revision": "4f4146d37ffc75a667d316a5aba4ed2e",
+    "url": "/monkey/static/media/user1.4f4146d3.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/monkey/precache-manifest.cd01a852a6e67063d92f68816755c3c3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monkey/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
